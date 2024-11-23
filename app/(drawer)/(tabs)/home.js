0a4a9c40ecc16df66f9b4c9730f1a46690953b2d
Krish <e63@bmsit.in>
2024-11-23 14:07:15 +0530

    Added cards in home page

--- app/(drawer)/(tabs)/home.js
+++ app/(drawer)/(tabs)/home.js
@@ -1,10 +1,70 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-
+import {View, Text, FlatList, Dimensions, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+const {height, width} = Dimensions.get('window');
 export default function Home() {
+  const [data, SetData] = useState([1, 1, 1, 1, 1]);
+  const [currentIndex, setCurrentIndex] = useState(0);
   return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-      <Text style={{fontSize:18}}>Home Page</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View
+        style={{
+          height: height / 2 + 100,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => index.toString()} // Assign a unique key
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          onScroll={e => {
+            const x = e.nativeEvent.contentOffset.x;
+            setCurrentIndex((x / width).toFixed(0));
+          }}
+          horizontal
+          renderItem={({item, index}) => {
+            return (
+              <View
+                style={{
+                  width: width - 50,
+                  height: height / 2,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <TouchableOpacity
+                  disabled={true}
+                  style={{
+                    width: '90%',
+                    height: '90%',
+                    backgroundColor: 'green',
+                    borderRadius: 10,
+                  }}></TouchableOpacity>
+              </View>
+            );
+          }}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: width,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {data.map((item, index) => {
+          return (
+            <View
+            key={index} // Assign a unique key for each indicator
+              style={{
+                width: currentIndex == index ? 50 : 8,
+                height: currentIndex == index ? 10 : 8,
+                borderRadius: currentIndex == index ? 5 : 4,
+                backgroundColor: currentIndex == index ? 'green' : 'gray',
+                marginLeft: 5,
+              }}></View>
+          );
+        })}
+      </View>
     </View>
-  )
+  );
 }
